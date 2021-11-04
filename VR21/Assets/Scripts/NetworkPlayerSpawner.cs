0abcccca7eacdd530da6d61f29ab22d3a91b6c12
Roman <bekkerrdm@gmail.com>
2021-11-05 02:11:47 +0300

    spawn player

---
+++ VR21/Assets/Scripts/NetworkPlayerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+
+    private GameObject spawnedPlayerPrefab;
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            Debug.Log("Connected PC.");
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Player PC", transform.position, transform.rotation);
+        }
+        else
+        {
+            Debug.Log("Connected VR.");
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Player VR", transform.position, transform.rotation);
+        }
+
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
+}
