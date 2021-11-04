962048318b7540ee31d2ea185693a5cf98ddceb6
Roman <bekkerrdm@gmail.com>
2021-11-05 02:42:24 +0300

    PC player sync

--- VR21/Assets/Scripts/PlayerPC.cs
+++ VR21/Assets/Scripts/PlayerPC.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPC : MonoBehaviour
 {
     public int speed;
+    private PhotonView photonView;
     void Start()
     {
         speed = 5;
+        photonView = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // передвижение персонажа
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        if (photonView.IsMine)
         {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-            var v = new Vector3(x, 0.0f, z);
-            transform.position += v * speed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            {
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
+                var v = new Vector3(x, 0.0f, z);
+                transform.position += v * speed * Time.deltaTime;
+            } 
         }
+        
     }
 }
