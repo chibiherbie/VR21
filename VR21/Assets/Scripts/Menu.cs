67c433f938352893a5328e6a99138bd9dda1762d
Roman <bekkerrdm@gmail.com>
2021-11-06 22:39:49 +0300

    RayCast menu

---
+++ VR21/Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private Canvas canvas = null;
+    private MenuManager menuManager = null;
+
+    private void Awake()
+    {
+        
+    }
+}
