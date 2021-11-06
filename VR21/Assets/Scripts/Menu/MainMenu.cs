d1304bb0ef8e54ba91600e72cdd02c66f0a65ea3
Roman <bekkerrdm@gmail.com>
2021-11-07 00:21:19 +0300

    Menu VR

---
+++ VR21/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button[] DisButton;
+    public void Start()
+    {
+        foreach (var item in DisButton)
+        {
+            item.interactable = false;
+            item.enabled = false;
+        }
+    }
+
+    public void Play()
+    {   
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ VR21/Assets/Scripts/TypePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class TypePlayer : MonoBehaviour
+{
+    
+    public GameObject VR;
+    public GameObject PC;
+
+    
+    void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            Debug.Log("Connected PC.");
+            Instantiate(PC, transform.position, transform.rotation);
+        }
+        else
+        {
+            Debug.Log("Connected VR.");
+
+        }
+    }
+}
