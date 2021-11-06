e80614ea9b8fa83ed582bd5d6ec0ef2646bdaa8c
konech <konovalovd02@gmial.com>
2021-11-07 01:01:28 +0700

    PcPlayer move(new)

--- VR21/Assets/Scripts/PlayerPC.cs
+++ VR21/Assets/Scripts/PlayerPC.cs
@@ -3,14 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Type { PlusPosition, ForseRigidbody }
+
 public class PlayerPC : MonoBehaviour
 {
+    /*Rigidbody player;
+    public float forse = 6;
     public int speed;
     private PhotonView photonView;
     void Start()
     {
         speed = 5;
         photonView = GetComponent<PhotonView>();
+        player = GetComponent<Rigidbody>();
+    }
+
+    bool is_ground = true;
+    void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("ground")) is_ground = true;
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("ground")) is_ground = false;
     }
 
     // Update is called once per frame
@@ -19,6 +34,7 @@ public class PlayerPC : MonoBehaviour
         // передвижение персонажа
         if (photonView.IsMine)
         {
+
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
             {
                 float x = Input.GetAxis("Horizontal");
@@ -28,5 +44,75 @@ public class PlayerPC : MonoBehaviour
             } 
         }
         
+        // jump pc Player
+        if (photonView.IsMine)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space) && is_ground)
+            {
+                player.AddForce(Vector3.up * forse, ForceMode.Impulse);
+                //float x = Input.GetAxis("Horizontal");
+                //float z = Input.GetAxis("Vertical");
+                //var v = new Vector3(x, 0.0f, z);
+                //photonView.AddForse(Vector2.up * forse);
+                //transform.position += v * speed * Time.deltaTime;
+                //transform.Translate(Vector3.up * -3.0f * Time.deltaTime , Space.World);
+            }
+        }
+
+
+    }*/
+
+    public Type TypeOf;
+    public Transform player;
+    public int speed;
+    public int jump;
+    
+    private void Start()
+    {
+        player = transform;
     }
+
+    private void Update()
+    {
+        if(TypeOf == Type.PlusPosition)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                player.position += player.forward * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.position -= player.forward * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                player.position += player.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                player.position -= player.right * speed * Time.deltaTime;
+            }
+        }
+        /*else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.position -= player.forward * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                player.position += player.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                player.position -= player.right * speed * Time.deltaTime;
+            }
+        }*/
+    }
+
 }
