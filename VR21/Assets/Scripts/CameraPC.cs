99282bfbe005634b56f636e0938d1032f1834bc6
konech <konovalovd02@gmial.com>
2021-11-07 22:25:43 +0700

    all move cherecter(final)

--- VR21/Assets/Scripts/CameraPC.cs
+++ VR21/Assets/Scripts/CameraPC.cs
@@ -1,4 +1,5 @@
 ﻿using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,9 @@ public class CameraPC : MonoBehaviour
     public GameObject player; // тут объект игрока
     private Vector3 offset;
     public GameObject Cam;
+    public float RotY;
+    public float RotX;
+    public Camera cameraTr;
 
     private PhotonView photonView;
     void Start()
@@ -18,7 +22,23 @@ public class CameraPC : MonoBehaviour
             Cam.gameObject.SetActive(false);
         }
     }
-        
+
+    private void Update()
+    {
+        MoveCam();
+    }
+
+    private void MoveCam()
+    {
+        RotY += Input.GetAxis("Mouse Y");
+        RotX += Input.GetAxis("Mouse X");
+
+        if (RotY < 18 && RotY > -45)
+        { 
+            cameraTr.transform.rotation = Quaternion.Euler(-RotY, RotX, 0f);   
+        }
+    }
+
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
